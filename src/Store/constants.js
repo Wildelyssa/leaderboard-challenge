42595213165ba276317b0daf41d0e24b74bb9265
Wilelyssa <wildelyssa@gmail.com>
2022-09-30 21:39:57 +0100

    replacing API with placeholder

--- src/Store/constants.js
+++ src/Store/constants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = `https://tribally.xyz/leaderboard/origin/arena`;
-export const ERROR_TEXT = 'Oops... Something went wrong!'
-export const REQUEST_PLAYER_DATA_IDLE = 'idle';
-export const REQUEST_PLAYER_DATA_PENDING = 'pending';
-export const REQUEST_PLAYER_DATA_SUCCESS = 'success';
-export const REQUEST_PLAYER_DATA_FAILED = 'failes';
+export const BASE_URL = `PLACEHOLDER`;
+// replace API here with mock
+export const ERROR_TEXT = "Oops... Something went wrong!";
+export const REQUEST_PLAYER_DATA_IDLE = "idle";
+export const REQUEST_PLAYER_DATA_PENDING = "pending";
+export const REQUEST_PLAYER_DATA_SUCCESS = "success";
+export const REQUEST_PLAYER_DATA_FAILED = "failes";
